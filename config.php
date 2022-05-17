e4dc76190b469ea99be9714bea5dbd2db5194a51
tegsa <michaldob514@gmail.com>
2022-05-17 13:00:30 +0200

    com

--- config.php
+++ config.php
@@ -3,8 +3,7 @@
 require('./vendor/autoload.php');
 
 $db = new mysqli("localhost", "root", "", "med");
-$smarty = new Smarty();
-
+$smarty = new Smarty(); 
 $smarty->setTemplateDir('./smarty/templates');
 $smarty->setCompileDir('./smarty/templates_c');
 $smarty->setCacheDir('./smarty/cache');
